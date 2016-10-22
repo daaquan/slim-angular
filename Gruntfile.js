d0424cbdcd27d914127ef1ba65fb662c1ad07c35
Alex Debril <alex.debril@gmail.com>
2016-10-22 22:54:13 +0200

    Grunt integration

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            min: {
+                files: {
+                    'public/app.min.js': ['node_modules/angular/angular.js', 'js/*.js']
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'public/style.min.css': ['node_modules/bootstrap/dist/css/bootstrap.css', 'css/*.css']
+                }
+            }
+        }
+    });
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};
