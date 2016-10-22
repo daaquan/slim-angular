9e7229d0f96000bf372ca3ea2e3f8458f409615a
Alex Debril <alex.debril@gmail.com>
2016-10-22 23:10:04 +0200

    rename src into php and move all PHP code in it

--- public/index.php
+++ public/index.php
@@ -14,17 +14,17 @@ require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require __DIR__ . '/../php/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+require __DIR__ . '/../php/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/../src/middleware.php';
+require __DIR__ . '/../php/middleware.php';
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
+require __DIR__ . '/../php/routes.php';
 
 // Run app
 $app->run();
