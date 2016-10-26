35d46b70e210b2e9f755a08636f4411d53ba8607
Alex Debril <alex.debril@gmail.com>
2016-10-26 17:31:56 +0200

    Tokenizer middleware

---
+++ php/app/Csrf/Tokenizer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Csrf;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class Tokenizer
+{
+    /**
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * Tokenizer constructor.
+     * @param string $prefix
+     */
+    public function __construct($prefix = 'csrf')
+    {
+        $this->prefix = $prefix;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param callable $next
+     * @return mixed
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        setcookie("csrf_token_name", $request->getAttribute("{$this->prefix}_name"));
+        setcookie("csrf_token_value", $request->getAttribute("{$this->prefix}_value"));
+
+        return $next($request, $response);
+    }
+
+}
--- php/middleware.php
+++ php/middleware.php
@@ -2,3 +2,6 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+$app->add(new \App\Csrf\Tokenizer);
+$app->add(new \Slim\Csrf\Guard);
