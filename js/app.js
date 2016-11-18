41696e45e113ec68d766ac61de3a37942f7ac520
alexdebril <alexdebril@users.noreply.github.com>
2016-11-18 14:26:48 +0100

    remove console.log()

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ var app = angular.module('myApp', ['ui.bootstrap', 'ngRoute', 'ngCookies']);
 var Tokenizer = function($cookies){
     return {
         tokenize: function(params) {
-            console.log(params);
             Object.assign(params, {
                 'csrf_name': $cookies.get('csrf_token_name'),
                 'csrf_value': $cookies.get('csrf_token_value')
